6c442e2c2e3f3fc563c9d96ce4ecbfae07bb6ee6
RoshanChampika1 <champikaroshan6@gmail.com>
2026-04-04 13:02:00 +0530

    Initial Commit

---
+++ SafeWalk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SafeWalk
+//
+//  Created by COBSCCOMP242P-064 on 2026-04-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SafeWalk/SafeWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeWalkApp.swift
+//  SafeWalk
+//
+//  Created by COBSCCOMP242P-064 on 2026-04-04.
+//
+
+import SwiftUI
+
+@main
+struct SafeWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
